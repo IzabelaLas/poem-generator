ca41e7f30c97ab099aca26fc3c8553dcea6d065b
Izabela <izabela.laszczuk@silksh.pl>
2023-12-28 16:28:20 +0100

    Connect to axios

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ const button = document.querySelector("#generate-button");
 const poem = document.querySelector("#poem-container");
 
 button.addEventListener("click", provideAnswer);
-let apiKey = "7a7cocba743t477eeab6780bf9e73b70";
-let context =
-  "make me laugh. Please provide the answer in basic HTML. Example <p>joke<p/>";
-let prompt = "Tell me a unique joke about IT.";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log(userInstructions.value);
 
 function provideAnswer(event) {
   event.preventDefault();
+
+  const userInstructions = document.querySelector("#text-input");
+  let apiKey = "7a7cocba743t477eeab6780bf9e73b70";
+  let context = "You are a romantic poet who likes to write 4 line poems.";
+  let prompt = `Provide me a French poem about: ${userInstructions.value}. Write the answer with basic HTML styling, so each line is written in the new line. Sign it at the end with 'SheCodes AI' Example: <strong>SheCodes AI</strong>`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   poem.innerHTML = "Loading a poem, please wait";
+
   axios
     .get(apiUrl)
     .then((response) => {
